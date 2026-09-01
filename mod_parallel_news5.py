4970b1bed7bdf040b6d5acef8cf69eeacb923e4a
tazgal <mailboxtasos@gmail.com>
2026-09-01 09:57:45 +0300

    Update Mega 104.6 station identifier

--- mod_parallel_news5.py
+++ mod_parallel_news5.py
@@ -285,7 +285,7 @@ def main():
             "Παραπολιτικά FM 90.1": "1986",
             "ERTnews Radio 105.8": "2073",
             "ΣΚΑΪ 100.3": "1334",
-            "Mega 104.6":"Mega"
+            "Mega 104.6":"4237"
         }
         
         selected_station_name = st.selectbox(
